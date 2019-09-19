0b6616873117f319504382edc05da7951fc07b26
bertech <santosc.aj@gmail.com>
2019-09-18 20:24:10 -0400

    organizing lint

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <el-container>
+        <el-header height="100px"> Notifications</el-header>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container> 
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </div>
-    <router-view />
   </div>
 </template>
 
-<style>
+<style scope>
+.el-header {
+    background-color: black;
+    color: white;
+    text-align: center;
+    line-height: 100px;
+    /* border: 3px solid gray; */
+    font-size:20px
+  }
+
+  .el-container{
+    /* border: 1px solid blue; */
+  }
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,7 +34,7 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
+/* #nav {
   padding: 30px;
 }
 
@@ -27,5 +45,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <Import/>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Import from "@/components/Import.vue"; // @ is an alias to /src
 
 export default Vue.extend({
   name: "home",
   components: {
-    HelloWorld
+    Import
   }
 });
 </script>
